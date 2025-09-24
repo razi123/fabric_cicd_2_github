a06fda4099ce16a8356b3e0c00c938ace831f9fa
Raziuddin Khazi <khazifb@gmail.com>
2025-09-24 15:52:48 +0200

    Committing 2 items from workspace c995b3b9-434b-4255-9848-874d1d26ff3e

--- fabric_items2/bronze_nb.Notebook/notebook-content.py
+++ fabric_items2/bronze_nb.Notebook/notebook-content.py
@@ -8,12 +8,12 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "d8a41520-fbd2-4395-8bba-4b99b9a05dcc",
+# META       "default_lakehouse": "e835c292-d746-4878-9dca-8758d7d3a7cf",
 # META       "default_lakehouse_name": "Lakehouse_Bronze",
-# META       "default_lakehouse_workspace_id": "664afdb0-9225-479c-8c53-a7a229002c5f",
+# META       "default_lakehouse_workspace_id": "c995b3b9-434b-4255-9848-874d1d26ff3e",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "d8a41520-fbd2-4395-8bba-4b99b9a05dcc"
+# META           "id": "e835c292-d746-4878-9dca-8758d7d3a7cf"
 # META         }
 # META       ]
 # META     }
@@ -39,7 +39,7 @@
 # file_path = "abfss://DEWorkshop_raziuddinkhazi_dev@onelake.dfs.fabric.microsoft.com/Lakehouse_Bronze.Lakehouse/Files/people-100.csv"
 # file_path = "abfss://DEWorkshop_raziuddinkhazi_feature@onelake.dfs.fabric.microsoft.com/Lakehouse_Bronze.Lakehouse/Files/people-100.csv"
 
-file_path = "Files/people-100.csv"
+file_path = "Files/raw-data/people-100.csv"
 # Read CSV into Spark DataFrame
 df = spark.read.format("csv") \
     .option("header", "true") \
@@ -47,6 +47,21 @@ df = spark.read.format("csv") \
     .load(file_path)
 
 display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Example: write DataFrame into Lakehouse managed tables
+df.write.format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("Lakehouse_Bronze.People_table")
+
 
 # METADATA ********************
 
