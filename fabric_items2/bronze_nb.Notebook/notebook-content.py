2cfcf43ce52cf0b56d47dfbeacc19afa1f96233d
Raziuddin Khazi <khazifb@gmail.com>
2025-09-25 11:27:23 +0200

    Committing 4 items from workspace 24fbb753-b211-47f0-9acf-ad7e07029fc8

--- fabric_items2/bronze_nb.Notebook/notebook-content.py
+++ fabric_items2/bronze_nb.Notebook/notebook-content.py
@@ -8,12 +8,12 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "e835c292-d746-4878-9dca-8758d7d3a7cf",
+# META       "default_lakehouse": "dd6f2bfb-b53b-4b9a-b9f3-88d5ae363985",
 # META       "default_lakehouse_name": "Lakehouse_Bronze",
 # META       "default_lakehouse_workspace_id": "24fbb753-b211-47f0-9acf-ad7e07029fc8",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "e835c292-d746-4878-9dca-8758d7d3a7cf"
+# META           "id": "dd6f2bfb-b53b-4b9a-b9f3-88d5ae363985"
 # META         }
 # META       ]
 # META     }
@@ -25,28 +25,6 @@
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
 
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-# Full Lakehouse ABFSS path
-# file_path = "abfss://DEWorkshop_raziuddinkhazi_dev@onelake.dfs.fabric.microsoft.com/Lakehouse_Bronze.Lakehouse/Files/people-100.csv"
-# file_path = "abfss://DEWorkshop_raziuddinkhazi_feature@onelake.dfs.fabric.microsoft.com/Lakehouse_Bronze.Lakehouse/Files/people-100.csv"
-
-# file_path = "Files/raw-data/people-100.csv"
-# # Read CSV into Spark DataFrame
-# df = spark.read.format("csv") \
-#     .option("header", "true") \
-#     .option("inferSchema", "true") \
-#     .load(file_path)
-
-# display(df)
 
 # METADATA ********************
 
@@ -75,8 +53,6 @@ def clean_column_names(df):
 
 # CELL ********************
 
-# Mask emails for privacy (show only first letter + domain)
-
 def mask_email(df):
     return df.withColumn(
         "email_masked",
@@ -96,7 +72,7 @@ def mask_email(df):
 # META }
 
 # CELL ********************
-# Calculate age from Date_of_birth
+
 def add_age(df):
     return df.withColumn(
         "age",
@@ -104,25 +80,6 @@ def add_age(df):
     )
 
 # METADATA ********************
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-# CELL ********************
-
-# -------------------------------
-# Apply transformations
-# -------------------------------
-# df_transformed = (
-#     df.transform(clean_column_names)
-#       .transform(mask_email)
-#       .transform(add_age)
-# )
-
-# display(df_transformed)
-
-
-# METADATA ********************
 
 # META {
 # META   "language": "python",
@@ -131,23 +88,8 @@ def add_age(df):
 
 # CELL ********************
 
-# Example: write DataFrame into Lakehouse managed tables
-# df_transformed.write.format("delta") \
-#     .mode("overwrite") \
-#     .saveAsTable("Lakehouse_Bronze.People_table")
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-# ------------------ only runs in Fabric, not during tests ------------------
 if __name__ == "__main__":
-    file_path = "Files/raw-data/people-100.csv"
+    file_path = "Files/people-100.csv"
 
     df = spark.read.format("csv") \
         .option("header", "true") \
@@ -165,7 +107,7 @@ if __name__ == "__main__":
     df_transformed.write.format("delta") \
         .mode("overwrite") \
         .saveAsTable("Lakehouse_Bronze.People_table")
-    
+
 # METADATA ********************
 
 # META {
--- fabric_items2/silver_nb.Notebook/notebook-content.py
+++ fabric_items2/silver_nb.Notebook/notebook-content.py
@@ -6,18 +6,7 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {
-# META     "lakehouse": {
-# META       "default_lakehouse": "e835c292-d746-4878-9dca-8758d7d3a7cf",
-# META       "default_lakehouse_name": "Lakehouse_Bronze",
-# META       "default_lakehouse_workspace_id": "24fbb753-b211-47f0-9acf-ad7e07029fc8",
-# META       "known_lakehouses": [
-# META         {
-# META           "id": "e835c292-d746-4878-9dca-8758d7d3a7cf"
-# META         }
-# META       ]
-# META     }
-# META   }
+# META   "dependencies": {}
 # META }
 
 # CELL ********************
@@ -37,16 +26,6 @@
 
 df = spark.sql("SELECT * FROM Lakehouse_Bronze.People_table LIMIT 1000")
 display(df)
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
 
 # METADATA ********************
 
