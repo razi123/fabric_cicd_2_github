76042db5abe160e12feee1ba481be0027fb71729
Raziuddin Khazi <khazifb@gmail.com>
2025-09-28 14:49:21 +0200

    Committing 3 items from workspace 24fbb753-b211-47f0-9acf-ad7e07029fc8

--- fabric_items2/bronze_nb.Notebook/notebook-content.py
+++ fabric_items2/bronze_nb.Notebook/notebook-content.py
@@ -6,7 +6,18 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "16db0e27-a7b9-4450-b893-ccf87626d560",
+# META       "default_lakehouse_name": "Lakehouse_Bronze",
+# META       "default_lakehouse_workspace_id": "24fbb753-b211-47f0-9acf-ad7e07029fc8",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "16db0e27-a7b9-4450-b893-ccf87626d560"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
@@ -108,6 +119,16 @@ if __name__ == "__main__":
     df_transformed.write.format("delta") \
         .mode("overwrite") \
         .saveAsTable("Lakehouse_Bronze.People_table")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
 
 # METADATA ********************
 
